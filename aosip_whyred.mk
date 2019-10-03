48bfb4232ed0c48431c89a270720ddca5fd84246
adi8900 <adrianszymanski242@gmail.com>
2019-10-03 07:01:33 +0000

    [DNM] whyred: Switch to passing safetynet fingerprint due to last google patch we need match fingerprint with security patch in rom it's temporary solution,looking for better one.
    Signed-off-by: adi8900 <adrianszymanski242@gmail.com>

--- aosip_whyred.mk
+++ aosip_whyred.mk
@@ -49,6 +49,6 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME="whyred" \
     PRIVATE_BUILD_DESC="whyred-user 9 PKQ1.180904.001 V10.3.1.0.PEICNXM release-keys"
 
-BUILD_FINGERPRINT := "xiaomi/whyred/whyred:9/PKQ1.180904.001/V10.3.1.0.PEICNXM:user/release-keys"
+BUILD_FINGERPRINT := "google/taimen/taimen:10/QP1A.190711.020/5800535:user/release-keys"
 
 TARGET_VENDOR := Xiaomi
